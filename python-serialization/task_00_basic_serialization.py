0bdd2c8f2391abd32d84d447b6efe9ec5f2f7e6b
Pierre1200 <rouvellat.p@icloud.com>
2026-05-27 09:18:52 +0200

    Update README and enhance serialization module documentation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,10 +5,18 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """
+    Sérialise un dictionnaire Python et le sauvegarde dans un fichier JSON.
+    Si le fichier existe déjà, il est automatiquement remplacé.
+    """
     with open("data.json", "w", encoding="utf-8") as file:
         json.dump(data, f, indent=4)
 
 
 def load_and_deserialize(filename):
+    """
+    Charge un JSON et le désérialise pour recréer le dictionnaire Python.
+    Retourne le dictionnaire Python reconstruit.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
