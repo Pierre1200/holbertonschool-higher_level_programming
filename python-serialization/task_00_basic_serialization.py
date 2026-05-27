8d126ce1cf76eb3dda4c4b2374aac08f3ec538be
Pierre1200 <rouvellat.p@icloud.com>
2026-05-27 09:20:52 +0200

    Fix filename parameter in serialize_and_save_to_file function

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,8 +9,8 @@ def serialize_and_save_to_file(data, filename):
     Sérialise un dictionnaire Python et le sauvegarde dans un fichier JSON.
     Si le fichier existe déjà, il est automatiquement remplacé.
     """
-    with open("data.json", "w", encoding="utf-8") as file:
-        json.dump(data, f, indent=4)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
 
 
 def load_and_deserialize(filename):
