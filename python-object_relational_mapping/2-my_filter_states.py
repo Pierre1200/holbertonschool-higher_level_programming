9a2eb84de61fec07f74c4c71fa81310117b2e4fb
Pierre1200 <rouvellat.p@icloud.com>
2026-06-08 15:48:50 +0200

    Add script to list states from the hbtn_0e_0_usa database

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Ce module liste tous les états de la base de données hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
+    cursor.execute(query.format(state_name_searched))
+    states = cursor.fetchall()
+    for row in states:
+        print("({}, '{}')".format(row[0], row[1]))
+    cursor.close()
+    db.close()
