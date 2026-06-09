944d31e210b57624f8c9de4a8edf2080a94c202f
Pierre1200 <rouvellat.p@icloud.com>
2026-06-09 10:24:20 +0200

    Add script to delete State objects containing 'a' from the database using SQLAlchemy

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Ce module utilise SQLAlchemy pour lister tous les objets State
+contenant la lettre 'a' presents dans la base de donnees.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, db_name
+    )
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in state_to_delete:
+        session.delete(state)
+    session.commit()
+    session.close()
