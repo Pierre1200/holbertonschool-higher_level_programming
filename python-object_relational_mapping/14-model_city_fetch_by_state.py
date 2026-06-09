81f508d6504e75fb89897ba9416ab86102ebcd5b
Pierre1200 <rouvellat.p@icloud.com>
2026-06-09 10:35:27 +0200

    Add import statement for City class in model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,6 +7,7 @@ import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_city import City
 
 if __name__ == "__main__":
     username = sys.argv[1]
