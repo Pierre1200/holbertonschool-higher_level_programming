02aeb0f1c62a7102bc20c4a74db2f63c378efae7
Pierre1200 <rouvellat.p@icloud.com>
2026-06-10 15:36:14 +0200

    Add status code printout in fetch_and_print_posts function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,6 +8,7 @@ def fetch_and_print_posts():
     """
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
+    print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
