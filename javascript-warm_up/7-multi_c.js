d54992dfb9f2e87c0836abb76d04452a251723a5
Pierre1200 <rouvellat.p@icloud.com>
2026-06-30 13:08:46 +0200

    Fix formatting by removing unnecessary newlines in 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,8 +3,7 @@
 const argv = process.argv;
 if (argv.length <= 2) {
   console.log('Missing number of occurrences');
-} 
-else {
+} else {
   const num = parseInt(argv[2]);
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
