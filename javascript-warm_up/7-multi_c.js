9e9547eb2515c081a02e5709d24edf0f19491072
Pierre1200 <rouvellat.p@icloud.com>
2026-06-30 13:05:51 +0200

    Add 7-multi_c.js to print 'C is fun' a specified number of times

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+if (argv.length <= 2) {
+  console.log('Missing number of occurrences');
+} 
+else {
+  const num = parseInt(argv[2]);
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
